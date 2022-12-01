ab6f4939174b700ca766d1a22780570d42135e24
Kennyd014 <david_kenny02@hotmail.com>
2022-12-01 16:35:56 -0300

    Create ejercicio1.js

---
+++ Mi-primer-proyecto-Node/ejercicio1.js
@@ -0,0 +1,15 @@
+const anioNac = new Date(1991, 2, 5);
+console.log(anioNac)
+
+
+
+const libroFavorito = {
+    titulo: "Time Machine",
+    autor: " H. G. Wells",
+    fechaLibro: 1895,
+    url: "https://en.wikipedia.org/wiki/The_Time_Machine"
+}
+misDatos = ["David", 31, false, anioNac, libroFavorito]
+console.log(libroFavorito.url)
+
+console.log(misDatos[4])
