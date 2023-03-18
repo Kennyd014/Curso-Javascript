1cf4145c28a56f5a4fe50760878fe67f2e4b0ba1
Kennyd014 <david_kenny02@hotmail.com>
2023-03-18 14:48:10 -0300

    Create Ejercicio String.js

---
+++ Mi-primer-proyecto-Node/Ejercicio String.js
@@ -0,0 +1,15 @@
+let nombre = "David";
+let apellido = "Kenny";
+let estudiante = `${nombre} ${apellido}`
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLocaleLowerCase()
+let estudianteTotal = estudiante.length
+let primaNombre = nombre.charAt(nombre)
+let ultiApellido = apellido.charAt(4)
+let sinEspacios = estudiante.trim()
+let bool = estudiante.includes("David")
+
+
+
+
+console.log(bool)
